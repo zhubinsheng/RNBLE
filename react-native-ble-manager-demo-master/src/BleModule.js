223b05bc290950163386a8d3c6336cb17ece4a40
吊不可言 <807124011@qq.com>
2020-05-23 19:52:52 +0800

    ok

--- react-native-ble-manager-demo-master/src/BleModule.js
+++ react-native-ble-manager-demo-master/src/BleModule.js
@@ -140,8 +140,8 @@ export default class BleModule{
         this.nofityServiceUUID = [];
         this.nofityCharacteristicUUID = [];
         for(let item of peripheralInfo.characteristics){
-            item.service = this.fullUUID(item.service);
-            item.characteristic = this.fullUUID(item.characteristic);
+            // item.service = this.fullUUID(item.service);
+            // item.characteristic = this.fullUUID(item.characteristic);
             if(Platform.OS == 'android'){
                 if(item.properties.Notify == 'Notify'){
                     this.nofityServiceUUID.push(item.service);
